cb6f0952e2cedec8d19c8fc1ef6612a365eac17f
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-14 22:14:31 -0500

    set up the index.js with import

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
